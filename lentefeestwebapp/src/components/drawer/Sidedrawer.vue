75454d5e45376287205a9e1a6ae8d608ce3e67df
minestarnl <bramkoene@gmail.com>
2019-11-06 00:15:27 +0100

    Now working

---
+++ lentefeestwebapp/src/components/drawer/Sidedrawer.vue
@@ -0,0 +1,36 @@
+
+
+    <template>
+  <v-list dense>
+    <v-list-item link>
+      <v-list-item-action>
+        <v-icon>mdi-home</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item link>
+      <v-list-item-action>
+        <v-icon>mdi-contact-mail</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>Contact</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+    
+    <script>
+export default {
+  name: "Sidedrawer",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
+    <style scoped>
+</style>

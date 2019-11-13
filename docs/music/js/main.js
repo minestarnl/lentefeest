f704f31169426d27ded0a4b7b2b302f66188da56
minestarnl <bramkoene@gmail.com>
2019-11-13 22:53:46 +0100

    added sass and debugger

--- docs/music/js/main.js
+++ docs/music/js/main.js
@@ -25,3 +25,13 @@ function change(sourceUrl) {
 $(document).ready(function() {
     $('.collapsible').collapsible();
 });
+
+var old = console.log;
+var logger = document.getElementById('log');
+console.log = function(message) {
+    if (typeof message == 'object') {
+        logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+    } else {
+        logger.innerHTML += message + '<br />';
+    }
+}
